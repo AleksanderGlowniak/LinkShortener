31766b7bc9f192da6ed136f1f745abc812a27ffc
Aleksander Glowniak <aleksander@users.norreplay.github.com>
2022-10-08 16:43:40 +0200

    Add Rest Controller

--- src/main/java/dev/greencashew/linkshortener/CreateLinkDto.java
+++ src/main/java/dev/greencashew/linkshortener/CreateLinkDto.java
@@ -1,8 +1,11 @@
 package dev.greencashew.linkshortener;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.time.LocalDate;
 
 record CreateLinkDto(
+        @Schema(description = "Identifier/alias to link. Used for redirection.", example = "link-alias", required = true)
     String id,
     String email,
     String targetUrl,
