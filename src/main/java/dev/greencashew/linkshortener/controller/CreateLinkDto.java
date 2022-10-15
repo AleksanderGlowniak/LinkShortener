91898a08fa2d9b60d33a8037ddcfa8b14916febe
Aleksander Glowniak <aleksander@users.norreplay.github.com>
2022-10-15 13:40:03 +0200

    Add validation

--- src/main/java/dev/greencashew/linkshortener/controller/CreateLinkDto.java
+++ src/main/java/dev/greencashew/linkshortener/controller/CreateLinkDto.java
@@ -3,13 +3,24 @@ package dev.greencashew.linkshortener.controller;
 import dev.greencashew.linkshortener.link.api.LinkDto;
 import io.swagger.v3.oas.annotations.media.Schema;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Future;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 record CreateLinkDto(
         @Schema(description = "Identifier/alias to link. Used for redirection.", example = "link-alias", required = true)
+                @NotBlank @Size(min = 1, max = 60)
     String id,
+    @Schema(description = "User email required for shortened link management (deletion, updating)", example = "test@greencashnew.dev", required = true)
+            @NotBlank @Email
     String email,
+    @Schema(description = "Destination url we would like to", example = "https://wp.pl/", required = true)
+            @NotBlank
     String targetUrl,
+    @Schema(description = "Link expiration time. If would like to have shortened link forever do not fill this field", example = "2022-01-01", required = false)
+            @Future
     LocalDate expirationDate){
 
     LinkDto toDto() {
